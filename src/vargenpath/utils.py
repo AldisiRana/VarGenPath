284f4f25df2d8cde581068da86a88a82c697b976
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2019-11-09 13:29:40 +0100

    update reader and gene retrieval functions

--- src/vargenpath/utils.py
+++ src/vargenpath/utils.py
@@ -2,33 +2,38 @@
 
 """Utilities for VarGenPath."""
 
-import pybiomart
+import pandas as pd
+from py2cytoscape.data.cyrest_client import CyRestClient
+from pybiomart import Dataset
+from urllib3.exceptions import NewConnectionError
 
 
 def file_reader(path: str) -> list:
-	"""
+    """
 	Read text file.
 	:param path: text file containing the variants
 	:return: list of variants
 	"""
-	file = open(path, "r")
-	content = [line for line in file.readline()]
-	return content
+    file = open(path, "r")
+    content = [line.strip() for line in file.readlines()]
+    return content
 
 
 def get_associated_genes(variants_list: list):
-	"""
-	Get associated gene information from BioMart.
+    """
+	Get variant gene information from BioMart.
 	:param variants_list: the list with variant ids.
-	:return: dataframe with gene information.
+	:return: dataframe with variant and gene information.
 	"""
-	server = pybiomart.Server(host='http://www.ensembl.org')
-	mart = server['ENSEMBL_MART_SNP']
-	dataset = mart['hsapiens_snp']
-	variant_gene_df = dataset.query(attributes=['refsnp_id', 'ensembl_gene_stable_id', 'associated_gene'],
-					filters={'snp_filter': variants_list})
-	return variant_gene_df
+    snp_dataset = Dataset(name='hsapiens_snp', host='http://www.ensembl.org')
+    variant_gene_df = snp_dataset.query(attributes=['refsnp_id', 'ensembl_gene_stable_id'],
+                                        filters={'snp_filter': variants_list})
+    gene_dataset = Dataset(name='hsapiens_gene_ensembl', host='http://www.ensembl.org')
+    gene_df = gene_dataset.query(attributes=['ensembl_gene_id', 'external_gene_name'], only_unique=False,
+                                 filters={'link_ensembl_gene_id': list(variant_gene_df['Gene stable ID'])})
+    merged_df = pd.merge(variant_gene_df, gene_df, on='Gene stable ID')
+    return merged_df
 
 
 def snps_genes_network(variants_genes_dict):
-	pass
+    pass
