9eef038a8e028fb037f879c43e1fb4648138bfea
AldisiRana <35561773+AldisiRana@users.noreply.github.com>
2019-11-10 13:28:32 +0100

    create function for network + update requirements

--- src/vargenpath/utils.py
+++ src/vargenpath/utils.py
@@ -4,8 +4,8 @@
 
 import pandas as pd
 from py2cytoscape.data.cyrest_client import CyRestClient
+import py2cytoscape.util.util_dataframe as df_util
 from pybiomart import Dataset
-from urllib3.exceptions import NewConnectionError
 
 
 def file_reader(path: str) -> list:
@@ -32,8 +32,19 @@ def get_associated_genes(variants_list: list):
     gene_df = gene_dataset.query(attributes=['ensembl_gene_id', 'external_gene_name'], only_unique=False,
                                  filters={'link_ensembl_gene_id': list(variant_gene_df['Gene stable ID'])})
     merged_df = pd.merge(variant_gene_df, gene_df, on='Gene stable ID')
+    interaction = ['association' for ind, row in merged_df.iterrows()]
+    merged_df['interaction'] = interaction
     return merged_df
 
 
-def snps_genes_network(variants_genes_dict):
-    pass
+def var_genes_network(variants_genes_df):
+    """Create cytoscape network from dataframe."""
+    try:
+        cy = CyRestClient()
+        cy.network.delete_all()
+        cy.session.delete()
+    except Exception as e:
+        raise Exception('Uh-oh! Make sure that cytoscape is open then try again.')
+    data = df_util.from_dataframe(variants_genes_df, source_col='Variant name', target_col='Gene name')
+    network = cy.network.create(data=data, name='VarGen network')
+    return network
