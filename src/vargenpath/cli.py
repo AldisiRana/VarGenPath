2469acb8e2e7c47822a9c73827c48eeb68b18e7f
AldisiRana <s0raldi@uni-bonn.de>
2019-11-13 12:32:38 +0100

    implement cli

--- src/vargenpath/cli.py
+++ src/vargenpath/cli.py
@@ -1,2 +1,37 @@
 # -*- coding: utf-8 -*-
 
+import click
+
+from .constants import LINKSET_PATH
+from .pipeline import get_vargenpath_network
+
+@click.command()
+@click.option('--variants-list', required=True, help='A list with variants.')
+@click.option('--network-name', default='VarGenPath network', help='The name of your network.')
+@click.option('--image-path', help='If input, the network will be saved as an image in the provided path.')
+@click.option('--session-path', help='If input, the cytoscape session will be saved in the provided path.')
+@click.option('--extend-network', is_flag=True, help='if true, the network will be extended.')
+@click.option('--linkset-path', Default=LINKSET_PATH,
+              help='if input network will be extended using the linkset, otherwise will use default.')
+def main(
+    variants_list,
+    netwok_name,
+    image_path,
+    session_path,
+    extend_network,
+    linkset_path,
+):
+    """Command Line Interface for VarGenPath."""
+    get_vargenpath_network(
+        variant_list_path=variants_list,
+        network_name=netwok_name,
+        image_path=image_path,
+        session_path=session_path,
+        extend_network=extend_network,
+        linkset_path=linkset_path,
+    )
+    click.echo('Task is done.')
+
+
+if __name__ == '__main__':
+    main()
--- src/vargenpath/pipeline.py
+++ src/vargenpath/pipeline.py
@@ -3,18 +3,21 @@
 """Pipeline for VarGenPath"""
 from typing import Optional
 
-from .constants import VARIANT_LIST_PATH
-from .utils import get_cytoscape_connection, file_reader, get_associated_genes, var_genes_network, \
-    extend_vargen_network, save_session, save_image
+from .constants import VARIANT_LIST_PATH, LINKSET_PATH
+from .utils import (
+    get_cytoscape_connection, file_reader, get_associated_genes, var_genes_network, extend_vargen_network, save_session,
+    save_image
+)
 
 
 def get_vargenpath_network(
         *,
         variant_list_path: str = VARIANT_LIST_PATH,
         network_name: Optional[str] = 'VarGenPath network',
-        linkset_path: Optional[str],
+        linkset_path: Optional[str] = LINKSET_PATH,
         session_path: Optional[str],
-        image_path: Optional[str]
+        image_path: Optional[str],
+        extend_network: bool = True,
 ):
     try:
         cy = get_cytoscape_connection()
@@ -23,7 +26,7 @@ def get_vargenpath_network(
     variants = file_reader(variant_list_path)
     vargen_df = get_associated_genes(variants)
     network = var_genes_network(variants_genes_df=vargen_df, client=cy, network_name=network_name)
-    if linkset_path is not None:
+    if extend_network:
         extend_vargen_network(linkset_path)
     if session_path is not None:
         save_session(session_file=session_path, client=cy)
